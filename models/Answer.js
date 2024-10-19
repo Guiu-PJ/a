b43f30731802684ac4c238b229e7e1effa4199c2
Guiu <guiup01@gmail.com>
2024-10-19 12:51:43 +0200

    aaaaaaaaAWFagewvh

--- models/Answer.js
+++ models/Answer.js
@@ -1,10 +1,12 @@
+import { answerGameIdAndQuestionIdFirebase } from '../BDD/Firebase.js';
 
 class Answer{
-    constructor(playerId, gameId, questionId, answer) {
+    constructor(playerId, gameId, questionId, questionTxt, answer) {
         this.id = this.generateUniqueId();
         this.playerId = playerId;
         this.gameId = gameId;
         this.questionId = questionId;
+        this.questionTxt = questionTxt;
         this.answer = answer;
     }
 
@@ -20,8 +22,23 @@ class Answer{
             playerId: this.playerId,
             gameId: this.gameId,
             questionId: this.questionId,
+            questionTxt: this.questionTxt,
             answer: this.answer,
         };
+    }
+
+    static async getByGameAndQuestion(gameId, questionId) {
+        try {
+            const querySnapshot = await answerGameIdAndQuestionIdFirebase(gameId, questionId);
+            const results = [];
+            querySnapshot.forEach((doc) => {
+                results.push(doc.data());  // Recoger los datos de cada documento y añadirlos al array
+            });
+            return results;  // Devolver los resultados como un array
+        } catch (error) {
+            console.error("Error fetching documents: ", error);
+            return [];
+        }
     }
 
 }
--- scripts/game.js
+++ scripts/game.js
@@ -3,10 +3,11 @@ import Game from '../models/Game.js';
 import { onSnapshot, updateDoc } from 'https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js';
 import Answer from '../models/Answer.js'
 
-let questions = [], currentQuestionIndex = null;
+let questions = [], currentQuestionIndex = null, answers = [];
 const gameId = sessionStorage.getItem('gameId');
 let game, gameDoc, gameRef, resp = 0, numPlayers = 0;
 const container = document.getElementById('a');
+const containerAnswers = document.getElementById('divRespuestas');
 
 document.addEventListener("DOMContentLoaded", async function () {
     try {
@@ -30,6 +31,12 @@ document.addEventListener("DOMContentLoaded", async function () {
         if (buttonNextQuestion) {
             buttonNextQuestion.addEventListener("click",handleNextQuestion);
         }
+
+        const buttonShowAnswers = document.getElementById("answers");
+        if (buttonShowAnswers) {
+            buttonShowAnswers.addEventListener("click", showAnswers);
+        }
+
     } catch (error) {
         console.error('Error en la inicialización:', error);
     }
@@ -37,34 +44,30 @@ document.addEventListener("DOMContentLoaded", async function () {
 
 async function displayPlayersInGame() {
     try {
-        if (gameDoc != null) {
-            if (gameDoc.exists()) {
-                const gameData = gameDoc.data();
-                container.innerHTML = '';
-                sessionStorage.setItem('modeId', gameData.modeId);
-                numPlayers = Object.keys(gameData.players).length;
-                console.log(gameData.players);
-                for (const playerId in gameData.players) {
-                    const playerDiv = document.createElement('div');
-                    playerDiv.textContent = `Jugador: ${gameData.players[playerId].name}`;
+        if (game != null) {
+            container.innerHTML = '';
+            sessionStorage.setItem('modeId', game.modeId);
+            numPlayers = Object.keys(game.players).length;
+            console.log(game.players);
+            for (const playerId in game.players) {
+                const playerDiv = document.createElement('div');
+                playerDiv.textContent = `Jugador: ${game.players[playerId].name}`;
 
-                    const deleteButton = document.createElement('button');
-                    deleteButton.textContent = 'Borrar';
+                const deleteButton = document.createElement('button');
+                deleteButton.textContent = 'Borrar';
+                deleteButton.id = 'divAdmin';
 
-                    deleteButton.addEventListener('click', async () => {
-                        console.log(`Botón del jugador ${gameData.players[playerId].name} presionado`);
-                        console.log(game);
-                        game.removePlayer(gameData.players[playerId].name);
-                        console.log(game);
-                        await game.save(false);
-                    });
+                deleteButton.addEventListener('click', async () => {
+                    console.log(`Botón del jugador ${game.players[playerId].name} presionado`);
+                    console.log(game);
+                    game.removePlayer(game.players[playerId].name);
+                    console.log(game);
+                    await game.save(false);
+                });
 
-                    playerDiv.appendChild(deleteButton);
+                playerDiv.appendChild(deleteButton);
 
-                    container.appendChild(playerDiv);
-                }
-            } else {
-                alert('No se encontró la partida.');
+                container.appendChild(playerDiv);
             }
         } else {
             alert('ID de partida no proporcionado.');
@@ -110,6 +113,52 @@ function nextQuestion(index) {
         console.log('No hay más preguntas.');
     }
 }
+async function getAnswers() {
+    if ((sessionStorage.getItem('playerName') === "admin")) {
+        document.getElementById('divRespuestas').style.display = 'block';
+        console.log(questions);
+        answers = await Answer.getByGameAndQuestion(game.id, questions[currentQuestionIndex].id);
+        console.log("respuestas = ", answers);
+        return answers;
+    }
+}
+
+async function showAnswers() {
+    let answers = await getAnswers();
+    let answerCount = {}; // Objeto para contar las respuestas
+    containerAnswers.innerHTML = '';
+    if ((sessionStorage.getItem('playerName') === "admin")) {
+        const answerButton = document.createElement('button');
+        answerButton.textContent = "Borrar";
+        answerButton.addEventListener('click', () => {
+            document.getElementById('divRespuestas').style.display = 'none';
+        });
+        containerAnswers.appendChild(answerButton);
+    }
+    // Primero contamos cuántas veces aparece cada respuesta
+    answers.forEach((answer) => {
+        if (answer.answer) {
+            if (answerCount[answer.answer]) {
+                answerCount[answer.answer]++;
+            } else {
+                answerCount[answer.answer] = 1;
+            }
+        }
+    });
+
+    // Luego mostramos las respuestas con su respectivo contador
+    for (const [answerText, count] of Object.entries(answerCount)) {
+        const answerDiv = document.createElement('div');
+        if (count > 1) {
+            answerDiv.textContent = `respuesta: ${answerText} x${count}`;
+        } else {
+            answerDiv.textContent = `respuesta: ${answerText}`;
+        }
+        answerDiv.id = answerText;
+        containerAnswers.appendChild(answerDiv);
+    }
+}
+   
 
 function setupSnapshotListener() {
     console.log("Configurando el listener de snapshots.");
@@ -127,6 +176,7 @@ function setupSnapshotListener() {
                     console.log("Estado del juego actualizado:", newGameStatus);
 
                     if (newGameStatus === "inGame") {
+                        await reloadGameDoc();
                         console.log("El juego ha comenzado.");
                         await startGame();
                     }
@@ -199,7 +249,7 @@ async function handlePlayerButtonClick(player, question) {
         console.log("Jugador seleccionado: " + player.name);
         let playerId = sessionStorage.getItem('playerId') || "admin";
         resp = 1;
-        const answer = new Answer(playerId, gameDoc.id, question.id, player.name);
+        const answer = new Answer(playerId, gameDoc.id, question.id, question.question, player.name);
         saveAnswer(answer)
         .then(() => {
             alert('¡Answer creada y guardada con éxito!');
@@ -213,5 +263,6 @@ async function handlePlayerButtonClick(player, question) {
 }
 
 async function reloadGameDoc() {
-    gameDoc = await getGameById(gameId);
+    //gameDoc = await getGameById(gameId);
+    game = await Game.getById(gameId);
 }
